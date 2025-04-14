878fa42da3615b33cb58e20e9456abd1bb530782
unknown <yshi.blanco@gmail.com>
2025-04-14 12:08:16 -0700

    added matrix implementation

---
+++ main/matrix.h
@@ -0,0 +1,16 @@
+#ifndef INC_MATRIX_H
+#define INC_MATRIX_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef int GridMatrix[8][8];
+
+extern GridMatrix playerGrid;
+extern GridMatrix attackGrid;
+
+uint8_t getRow(GridMatrix matrix, int row);
+void resetMatrix(GridMatrix matrix);
+void updateMatrix(GridMatrix matrix, int shipLength, int cursorX, int cursorY, bool vertical);
+
+#endif
