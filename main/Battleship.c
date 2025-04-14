f15122df22a319a64f1cbff7e2f0c5bed35f689c
unknown <yshi.blanco@gmail.com>
2025-04-14 15:37:56 -0700

    added GPIO initializations for buttons

--- main/Battleship.c
+++ main/Battleship.c
@@ -1,5 +1,7 @@
 #include "sdkconfig.h"
 #include "espnow_comms.h"
+#include "display.h"
+#include "matrix.h"
 #include <stdio.h>
 #include <stdint.h>
 #include "freertos/FreeRTOS.h"
@@ -7,8 +9,36 @@
 #include "nvs_flash.h"
 #include "driver/gpio.h"
 
+//GPIO pins for buttons
+#define CONFIRM_BUTTON  38
+#define ROTATE_BUTTON   33
+#define UP_BUTTON       34
+#define RIGHT_BUTTON    35
+#define DOWN_BUTTON     36
+#define LEFT_BUTTON     37   
+
 void createTasks(void) {
     xTaskCreatePinnedToCore(handleDataTask, "handle_data", 4096, NULL, 0, NULL, 0);
+}
+
+void initGPIO(void) {
+    gpio_set_direction(CONFIRM_BUTTON, GPIO_MODE_INPUT);
+    gpio_pullup_en(CONFIRM_BUTTON);
+
+    gpio_set_direction(ROTATE_BUTTON, GPIO_MODE_INPUT);
+    gpio_pullup_en(ROTATE_BUTTON);
+
+    gpio_set_direction(UP_BUTTON, GPIO_MODE_INPUT);
+    gpio_pullup_en(UP_BUTTON);
+
+    gpio_set_direction(RIGHT_BUTTON, GPIO_MODE_INPUT);
+    gpio_pullup_en(RIGHT_BUTTON);
+
+    gpio_set_direction(DOWN_BUTTON, GPIO_MODE_INPUT);
+    gpio_pullup_en(DOWN_BUTTON);
+
+    gpio_set_direction(LEFT_BUTTON, GPIO_MODE_INPUT);
+    gpio_pullup_en(LEFT_BUTTON);
 }
 
 void app_main(void) {
@@ -22,7 +52,29 @@ void app_main(void) {
 
     initWifi();
     initESPNOW();
+    initSPI();
+    initGPIO();
+    initDisplays();
     createTasks();
+
+    while(1){
+        //transmitData();
+        //vTaskDelay(pdMS_TO_TICKS(1000));  // Wait 1 second to avoid watchdog timeout
+        if (gpio_get_level(CONFIRM_BUTTON) == 0) {
+            printf("Confirm pressed!\n");
+        } else if (gpio_get_level(ROTATE_BUTTON) == 0) {
+            printf("Rotate pressed!\n");
+        } else if (gpio_get_level(UP_BUTTON) == 0) {
+            printf("Up pressed!\n");
+        } else if (gpio_get_level(RIGHT_BUTTON) == 0) {
+            printf("Right pressed!\n");
+        } else if (gpio_get_level(DOWN_BUTTON) == 0) {
+            printf("Down pressed!\n");
+        } else if (gpio_get_level(LEFT_BUTTON) == 0) {
+            printf("Left pressed!\n");
+        } 
+        vTaskDelay(10);
+    }
 
 
 }
