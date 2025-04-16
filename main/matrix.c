344c49388c41663f5bdbafd6cd553c3f3c5203e8
unknown <yshi.blanco@gmail.com>
2025-04-16 13:07:26 -0700

    bug fixes

--- main/matrix.c
+++ main/matrix.c
@@ -24,12 +24,12 @@ GridMatrix attackGrid = {
     {0,0,0,0,0,0,0,0},
 };
 
-uint8_t getRow(GridMatrix matrix, int row) {
+uint8_t getRow(GridMatrix* matrix, int row) {
     int col;
     uint8_t data = 0;
 
     for (col = 0; col <= 7; col++) {
-        if (matrix[row][col] == 1) {
+        if ((*matrix)[row][col] == 1) {
             data |= (1 << (7 - col));
         }
     }
@@ -37,26 +37,26 @@ uint8_t getRow(GridMatrix matrix, int row) {
     return data;
 }
 
-void resetMatrix(GridMatrix matrix) {
+void resetMatrix(GridMatrix* matrix) {
     int i, j;
     for (i = 0; i <= 7; i++) {
         for (j = 0; j <=7; j++) {
-            matrix[i][j] = 0;
+            (*matrix)[i][j] = 0;
         }
     }
 }
 
-void updateMatrix(GridMatrix matrix, int shipLength, int cursorX, int cursorY, bool vertical) {
+void updateMatrix(GridMatrix* matrix, int shipLength, int cursorX, int cursorY, bool vertical) {
     resetMatrix(matrix);
     int i;
 
     if (vertical) {
-        for (i = cursorY; i < cursorY - shipLength; i--) {
-            matrix[i][cursorX] = 1;
+        for (i = cursorY; i > cursorY - shipLength; i--) {
+            (*matrix)[i][cursorX] = 1;
         }
     } else {
         for (i = cursorX; i < cursorX + shipLength; i++) {
-            matrix[cursorY][i] = 1;
+            (*matrix)[cursorY][i] = 1;
         }
     }
 }
--- main/matrix.h
+++ main/matrix.h
@@ -9,8 +9,8 @@ typedef int GridMatrix[8][8];
 extern GridMatrix playerGrid;
 extern GridMatrix attackGrid;
 
-uint8_t getRow(GridMatrix matrix, int row);
-void resetMatrix(GridMatrix matrix);
-void updateMatrix(GridMatrix matrix, int shipLength, int cursorX, int cursorY, bool vertical);
+uint8_t getRow(GridMatrix* matrix, int row);
+void resetMatrix(GridMatrix* matrix);
+void updateMatrix(GridMatrix* matrix, int shipLength, int cursorX, int cursorY, bool vertical);
 
 #endif
