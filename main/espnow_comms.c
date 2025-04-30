1a430cc8f0a4102197d7eacf3978364baadf5079
unknown <yshi.blanco@gmail.com>
2025-04-30 15:28:10 -0700

    added functions for sending different data over ESP-NOW

--- main/espnow_comms.c
+++ main/espnow_comms.c
@@ -1,4 +1,7 @@
 #include "espnow_comms.h"
+#include "display.h"
+#include "attack_logic.h"
+#include "game_logic.h"
 #include "esp_mac.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
@@ -9,6 +12,7 @@
 #include "freertos/queue.h"
 #include "esp_log.h"
 #include <string.h>
+#include <stdbool.h>
 
 
 static const char* TAG = "ESP-NOW";
@@ -16,7 +20,6 @@ QueueHandle_t dataQueue;
 const uint8_t peerMac[6] = {0x70, 0x04, 0x1D, 0xF7, 0xB3, 0xEE};
 
 void initWifi(void) {
-
     //Initializing WiFi (needed for ESP-NOW)
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -69,23 +72,63 @@ void initESPNOW(void) {
 
     free(peerInfo);                                                 //freeing up the memory we allocated earlier (must be done when dynamically allocating memory)
 
-    //Creating queue of size 5 for handling data sent over ESP-NOW
-    dataQueue = xQueueCreate(5, sizeof(uint8_t));
+    //Creating queue of size 4 for handling data sent over ESP-NOW
+    dataQueue = xQueueCreate(4, sizeof(GameData));
 
 }
 
-void sendData(uint8_t* data, size_t dataSize) {
-    esp_now_send(peerMac, data, dataSize);
+void sendReadyStatusData(bool isReady, uint32_t randomNumber) {
+    GameData data = {
+        .msg = MSG_READY,
+        .message.readyStatusData = {
+            .ready = isReady,
+            .diceRoll = randomNumber
+        }
+    };
+
+    esp_now_send(peerMac, (uint8_t*)&data, sizeof(GameData));
+}
+
+void sendAttackData(int x, int y) {
+    GameData data = {
+        .msg = MSG_ATTACK,
+        .message.attackData = {
+            .x = x,
+            .y = y
+        }
+    };
+
+    esp_now_send(peerMac, (uint8_t*)&data, sizeof(GameData));
+}
+
+void sendHitData(bool wasHit) {
+    GameData data = {
+        .msg = MSG_HIT,
+        .message.hitData = {
+            .hit = wasHit
+        }
+    };
+
+    esp_now_send(peerMac, (uint8_t*)&data, sizeof(GameData));
+}
+
+void sendWinConditionData(bool status) {
+    GameData data = {
+        .msg = MSG_WIN_CONDITION,
+        .message.winConditionData = {
+            .allSunk = status
+        }
+    };
+
+    esp_now_send(peerMac, (uint8_t*)&data, sizeof(GameData));
 }
 
 void handleDataTask(void* pvParameters) {
 
-    uint8_t receivedData;
+    GameData receivedData;
 
     while(1) {
-        if (xQueueReceive(dataQueue, &receivedData, portMAX_DELAY) == pdTRUE) {
-            printf("Data received: %d", receivedData);          //test
-        }
+        
     }
 
     vTaskDelete(NULL);
--- main/espnow_comms.h
+++ main/espnow_comms.h
@@ -3,7 +3,38 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 #include "esp_now.h"
+
+typedef enum {
+    MSG_READY,
+    MSG_ATTACK,
+    MSG_HIT,
+    MSG_WIN_CONDITION
+} MessageType;
+
+typedef struct {
+    MessageType msg;
+    union {
+        struct {
+            bool ready;
+            int diceRoll;
+        } readyStatusData;
+
+        struct {
+            int x;
+            int y;
+        } attackData;
+
+        struct {
+            bool hit;
+        } hitData;
+
+        struct {
+            bool allSunk;
+        } winConditionData;
+    } message;
+} GameData;
 
 extern const uint8_t peerMac[6];
 
@@ -11,7 +42,10 @@ void initWifi(void);
 void initESPNOW(void);
 void onSend(const uint8_t* data, esp_now_send_status_t status);
 void onReceive(const esp_now_recv_info_t* info, const uint8_t* receivedData, int lenData);
-void sendData(uint8_t* data, size_t dataSize);
+void sendReadyStatusData(bool isReady, uint32_t randomNumber);
+void sendAttackData(int x, int y);
+void sendHitData(bool wasHit);
+void sendWinConditionData(bool status);
 void handleDataTask(void* pvParameters);
 
 #endif
